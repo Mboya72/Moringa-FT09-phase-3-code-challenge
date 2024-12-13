a1bf3bb0c498c4424e1fd45ab91ff4f015d42ebe
Mboya72 <elvindio72@gmail.com>
2024-12-13 18:22:20 +0300

    Adding Article model with relationships to Author and Magazine, and title validation

--- models/article.py
+++ models/article.py
@@ -3,13 +3,24 @@ from sqlalchemy.orm import relationship
 from database.connection import Base
 
 
-class Article:
-    def __init__(self, id, title, content, author_id, magazine_id):
-        self.id = id
-        self.title = title
+class Article(Base):
+    __tablename__ = 'articles'
+
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    _title = Column('title', String, nullable=False)
+    content = Column(String, nullable=True)
+    author_id = Column(Integer, ForeignKey('authors.id'), nullable=False)
+    magazine_id = Column(Integer, ForeignKey('magazines.id'), nullable=False)
+
+    author = relationship("Author", backref="articles")
+    magazine = relationship("Magazine", backref="articles")
+
+    def __init__(self, author, magazine, title, content):
+        self.author = author
+        self.magazine = magazine
+        self.title = title  
         self.content = content
-        self.author_id = author_id
-        self.magazine_id = magazine_id
+
 
     def __repr__(self):
         return f'<Article {self.title}>'
